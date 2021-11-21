daaaba2ccc59be2bb9a22284c2dc9a548f7af295
SavaFeeD <shachnev.02@mail.ru>
2021-11-22 05:09:24 +0700

    update

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
+from hashlib import sha256
+
+
 def proceed_release(body, chat_id):
     print(body, chat_id)
+
+
+def get_hash(value):
+    return sha256(value.encode('utf-8')).hexdigest()
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from starlette import status
 from starlette.responses import Response
-import functions
+import functions as func
 from models import Body
 
 app = FastAPI()
+
+
+@app.get("/")
+async def general():
+    return Response(content={'key': func.get_hash('key')}, status_code=status.HTTP_200_OK)
 
 
 @app.post("/release/")
 async def release(*,
                   body: Body,
                   chat_id: str = None):
-    await functions.proceed_release(body, chat_id)
+    await func.proceed_release(body, chat_id)
     return Response(status_code=status.HTTP_200_OK)
