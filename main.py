ad897ce961c434afee776f5cad21a3b4750b280e
SavaFeeD <shachnev.02@mail.ru>
2021-11-22 05:36:24 +0700

    update

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from fastapi import FastAPI
-from starlette import status
-from starlette.responses import Response
-import functions as func
-from models import Body
+from router import router
 
 app = FastAPI()
 
-
-@app.get("/")
-async def general():
-    return Response(content={'key': func.get_hash('key')}, status_code=status.HTTP_200_OK)
-
-
-@app.post("/release/")
-async def release(*,
-                  body: Body,
-                  chat_id: str = None):
-    await func.proceed_release(body, chat_id)
-    return Response(status_code=status.HTTP_200_OK)
+router(app)
