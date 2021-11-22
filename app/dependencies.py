b2e7df2e136aa02d07ff3377f36e931171c6a150
SavaFeeD <shachnev.02@mail.ru>
2021-11-22 12:22:17 +0700

    update

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,10 @@
 from fastapi import Header, HTTPException
+from fastapi.templating import Jinja2Templates
+
 from typing import Optional
+
+
+templates = Jinja2Templates(directory='./app/templates')
 
 
 async def get_token_header(x_token: Optional[str] = Header(None)):
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,15 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
 from starlette import status
 from starlette.responses import JSONResponse
 
 from app.schemes import User
+from app.dependencies import templates
+
 
 router = APIRouter(
     prefix='/user',
     tags=['user'],
     responses={404: {'descriptions': 'Not Found'}},
 )
+
+
+@router.get("/login", response_class=HTMLResponse)
+async def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
 
 
 @router.post("/login")
